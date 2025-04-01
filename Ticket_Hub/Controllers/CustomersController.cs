7722c7b01580ae17de96ce287608f88e7fc11248
Mitchell Yetman <w0498497@nscc.ca>
2025-04-01 20:37:21 -0300

    created purchase model and API. tested and working correctly as far as I can tell

---
+++ Ticket_Hub/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ticket_Hub.Models;
+
+namespace Ticket_Hub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ILogger<CustomersController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public CustomersController(ILogger<CustomersController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult SendCustomerDataToAzure (Purchase purchaseRequest)
+        {
+            if (ModelState.IsValid)
+            {
+
+                return Ok("Successful post for " + purchaseRequest.name);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Ticket_Hub/Models/Purchase.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ticket_Hub.Models
+{
+    public class Purchase
+    {
+        public int concertId;
+
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Invalid name.")]
+        public string name { get; set; }
+
+        [Required]
+        [Phone]
+        public string phone { get; set; }
+
+        [Required]
+        [Range(1, 50)]
+        public int quantity { get; set; }
+
+        [Required]
+        [CreditCard]
+        public string creditCard { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(0[1-9]|1[0-2])(2[5-9])$", ErrorMessage = "Invalid expiration date.")]
+        public string expiration { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{3}$", ErrorMessage = "Invalid security code.")]
+        public string securityCode { get; set; }
+
+        [Required]
+        public string address { get; set; }
+
+        [Required]
+        public string city { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Invalid province name.")]
+        [StringLength(25, MinimumLength = 2)]
+        public string province { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$", ErrorMessage = "Invalid postal code.")]
+        public string postalCode { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Invalid country name.")]
+        [StringLength(25, MinimumLength = 2)]
+        public string country { get; set; }
+    }
+}
